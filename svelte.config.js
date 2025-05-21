f9a326cce8ac5d9741eb30fde3465b1f33079e6e
Your Name <you@example.com>
2025-05-21 18:19:04 +0800

    Fix Netlify adapter and remove multiple default exports

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
 // svelte.config.js
-import adapter from '@sveltejs/adapter-auto'; // or your specific adapter
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(), // This will pick up PostCSS config by default
+  preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter(),
-    // ... other kit config
+    // other kit config here if needed
   }
 };
 
 export default config;
+git status
